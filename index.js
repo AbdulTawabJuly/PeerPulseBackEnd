121037a69d22fc966cb795c220c2bc3e859ede39
AbdulTawabJuly <tawabmasood31july@gmail.com>
2023-09-20 22:07:15 +0500

    Login

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-const { Server } = require('socket.io');
-const mongoose = require('mongoose');
-const auth = require('./routes/auth');
-const rooms = require('./routes/rooms');
-require('dotenv').config();
+const { Server } = require("socket.io");
+const mongoose = require("mongoose");
+const auth = require("./routes/auth");
+const rooms = require("./routes/rooms");
+const cors = require("cors");
+require("dotenv").config();
 
-
+app.use(cors()); // THis allows all origin
 app.use(express.json());
-app.use('/api/auth',auth);
+app.use("/api/auth", auth);
+
 //app.use('/api/room',rooms);
 
 
-const io = new Server(server)
+const io = new Server(server);
 const PORT = process.env.PORT || 8080;
 
-io.on("connection", (socket)=> {
+io.on("connection", (socket) => {
   console.log("user connected");
-})
+});
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(()=> {
-      server.listen(PORT, () => {
-        console.log("listening on PORT: ",PORT, ' and connected to database');
-      });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log("listening on PORT: ", PORT, " and connected to database");
+    });
   })
-  .catch((error)=> {
-    console.log('error occured connecting to mongoose');
-  })
-
-
+  .catch((error) => {
+    console.log("error occured connecting to mongoose");
+  });
