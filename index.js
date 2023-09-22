2127de2193545650a0dbf4779fecae1c67e6ffb1
AbdulTawabJuly <tawabmasood31july@gmail.com>
2023-09-22 11:40:39 +0500

    Login/Signup Complete

--- index.js
+++ index.js
@@ -9,22 +9,16 @@ const auth = require("./routes/auth");
 const rooms = require("./routes/rooms");
 const cors = require("cors");
 require("dotenv").config();
-
-// app.use(bodyParser.urlencoded());
+// app.use(bodyParser.json());
 app.use(cors()); // This allows all origin
-
 app.use(express.json());
 app.use("/api/auth", auth);
-
 //app.use('/api/room',rooms);
-
 const io = new Server(server);
 const PORT = process.env.PORT || 8080;
-
 io.on("connection", (socket) => {
   console.log("user connected");
 });
-
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
