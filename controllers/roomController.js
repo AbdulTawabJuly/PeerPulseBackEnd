5fe49a948e79494488875283145a07b530bfc8dd
ameerhamza-23 <hamzaiscoding@gmail.com>
2023-09-30 20:48:46 +0500

    Initialize socket

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -38,10 +38,8 @@ const SearchRoom = async (req, res) => {
 }
 else
 {
-    console.log('all');
     try{
     const searchResult = await Room.find({isPublic:true});
-    console.log(searchResult);
     if (searchResult) {
         res.status(200).json(searchResult);
       } else {
@@ -55,15 +53,11 @@ else
 
 const DeleteExpiredRooms=async()=>{
   const currentTime = Date.now();
-console.log('Current time:', new Date(currentTime).toISOString());  // toISOString() Function converts Date into String
 
 const oneHourAgo = new Date(currentTime - 1000 * 60 * 60);
-console.log('One hour ago:', oneHourAgo.toISOString());
 
-  console.log(currentTime);
    try{
     const response=await Room.deleteMany({startingTime:{$lt:oneHourAgo}});
-    console.log('deleted Successfully');
    }
    catch(error){
     console.log(error);
