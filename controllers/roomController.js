0730032608d6552935572f0a08cce662a1b68f72
AbdulTawabJuly <tawabmasood31july@gmail.com>
2023-10-27 20:34:35 +0500

    Invoice Template

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,4 +1,5 @@
 //room controller
+const { sendMail,invoiceTemplate } = require("../Extras/common");
 
 const Room = require("../models/Room");
 const createRoom = async (req, res) => {
@@ -12,37 +13,26 @@ const createRoom = async (req, res) => {
       name: RoomName,
       createdBy: user,
       members: [],
-      startingTime:Date.now(),
-      isPublic:req.body.isPublic,
-      isPaid:req.body.isPaid,
-      price:req.body.price,
+      startingTime: Date.now(),
+      isPublic: req.body.isPublic,
+      isPaid: req.body.isPaid,
+      price: req.body.price,
     });
     res.status(200).json(TRoom);
   } catch (err) {
-    console.log({error:"Error Creating Room"});
+    console.log({ error: "Error Creating Room" });
   }
 };
 
 const SearchRoom = async (req, res) => {
   const Rooms = req.query.RoomName;
-  if(Rooms)
-  {
-  try {
-    const searchResult = await Room.find( { name:{$regex:'.*'+Rooms+'.*'},isPublic:true} );  // this is LIKE command in SQL
-    if (searchResult) {
-      res.status(200).json(searchResult);
-    } else {
-      res.json({ NoResults: "No results found" });
-    }
-  } catch (err) {
-    res.status(500).json({ error: err });
-  }
-}
-else
-{
-    try{
-    const searchResult = await Room.find({isPublic:true});
-    if (searchResult) {
+  if (Rooms) {
+    try {
+      const searchResult = await Room.find({
+        name: { $regex: ".*" + Rooms + ".*" },
+        isPublic: true,
+      }); // this is LIKE command in SQL
+      if (searchResult) {
         res.status(200).json(searchResult);
       } else {
         res.json({ NoResults: "No results found" });
@@ -50,84 +40,132 @@ else
     } catch (err) {
       res.status(500).json({ error: err });
     }
-}
+  } else {
+    try {
+      const searchResult = await Room.find({ isPublic: true });
+      if (searchResult) {
+        res.status(200).json(searchResult);
+      } else {
+        res.json({ NoResults: "No results found" });
+      }
+    } catch (err) {
+      res.status(500).json({ error: err });
+    }
+  }
 };
 
-const DeleteExpiredRooms=async()=>{
+const DeleteExpiredRooms = async () => {
   const currentTime = Date.now();
 
-const oneHourAgo = new Date(currentTime - 1000 * 60 * 60);
+  const oneHourAgo = new Date(currentTime - 1000 * 60 * 60);
 
-   try{
-    const response=await Room.deleteMany({startingTime:{$lt:oneHourAgo}});
-   }
-   catch(error){
+  try {
+    const response = await Room.deleteMany({
+      startingTime: { $lt: oneHourAgo },
+    });
+  } catch (error) {
     console.log(error);
-   }
-}
-const JoinRoom=async(req,res)=>{
-   
-  const roomID=String(req.query.RoomID.id);
-  const user=req.query.UserID;
+  }
+};
+const JoinRoom = async (req, res) => {
+  const roomID = String(req.query.RoomID.id);
+  const user = req.query.UserID;
 
-   try{
-    const response=await Room.findOne({members:user});
-    var response1,response2,response3;
-    if(!response)
-    {
-       response2=await Room.updateOne({_id:roomID},{$push:{members:user}});
-       response3=await Room.updateOne({_id:roomID},{$inc:{noOfMembers:1}});
+  try {
+    const response = await Room.findOne({ members: user });
+    var response1, response2, response3;
+    if (!response) {
+      response2 = await Room.updateOne(
+        { _id: roomID },
+        { $push: { members: user } }
+      );
+      response3 = await Room.updateOne(
+        { _id: roomID },
+        { $inc: { noOfMembers: 1 } }
+      );
     }
-    response1=await Room.findOne({_id:roomID}).populate('members','_id email'); 
-    if(response1)
-    {
+    response1 = await Room.findOne({ _id: roomID }).populate(
+      "members",
+      "_id email"
+    );
+    if (response1) {
       res.status(200).json(response1);
+    } else {
+      res.status(404).json({ error: "No Room found." });
     }
-    else{
-      res.status(404).json({error:"No Room found."});
-    }
-   }
-   catch(err)
-   {
+  } catch (err) {
     console.log(err);
-      res.status(500).json({error:"Error Finding Room"}); 
-   }
-}
-const LeaveRoom=async(req,res)=>{
+    res.status(500).json({ error: "Error Finding Room" });
+  }
+};
+const LeaveRoom = async (req, res) => {
   const Roomid = String(req.query.RoomID.id);
   const user = String(req.query.currentUser);
-  try{
-    const response=await Room.findOne({members:user});
-    var response1,response2;
-    if(response)
-    {
-       response1=await Room.updateOne({_id:Roomid},{$pull:{members:user}});
-       response2=await Room.updateOne({_id:Roomid},{$inc:{noOfMembers:-1}});
-    }  
-    if(response1&&response2)
-    {
-      res.status(200).json({success:"All OK!"});
+  try {
+    const response = await Room.findOne({ members: user });
+    var response1, response2;
+    if (response) {
+      response1 = await Room.updateOne(
+        { _id: Roomid },
+        { $pull: { members: user } }
+      );
+      response2 = await Room.updateOne(
+        { _id: Roomid },
+        { $inc: { noOfMembers: -1 } }
+      );
     }
-  }
-  catch(error)
-  {
+    if (response1 && response2) {
+      res.status(200).json({ success: "All OK!" });
+    }
+  } catch (error) {
     console.log(error);
-    res.status(500).json({error:"Error Leaving Room"});
+    res.status(500).json({ error: "Error Leaving Room" });
   }
-}
+};
 
-const getRoom = async(req,res)=> {
+const getRoom = async (req, res) => {
   const Roomid = String(req.query.roomID);
   try {
-    const response = await Room.findOne({_id:Roomid}).populate('members','_id email');;
-    if(response) {
+    const response = await Room.findOne({ _id: Roomid }).populate(
+      "members",
+      "_id email"
+    );
+    if (response) {
       res.status(200).json(response);
     }
-  }
-  catch (error) {
+  } catch (error) {
     console.log(error);
-    res.status(500).json({error:'could not get room'});
+    res.status(500).json({ error: "could not get room" });
   }
   return res.status(200);
-}
-module.exports = { createRoom,SearchRoom,DeleteExpiredRooms,JoinRoom,LeaveRoom, getRoom };
+};
+
+const sendInvoice = async (req, res) => {
+  const { name, price } = req.body.RoomDetails;
+  const email = req.body.user.user.email;
+  const id = req.body.params.id;
+  // console.log(name,price,email,id);
+  const subject = "Payment Invoice";
+  const text =
+    "Your Payment for Room: " + name + " of " + price + " has been recieved.";
+  // const html = `<p>Your Payment for Room: "${name}" of ${price} has been Recieved. Thank you </p>`;
+  if (email) {
+    const response = await sendMail({
+      to: email,
+      subject,
+      text,
+      html:invoiceTemplate(id,name,price),
+    });
+  } else {
+    res.sendStatus(400);
+  }
+};
+module.exports = {
+  createRoom,
+  SearchRoom,
+  DeleteExpiredRooms,
+  JoinRoom,
+  LeaveRoom,
+  getRoom,
+  sendInvoice,
+};
--- routes/rooms.js
+++ routes/rooms.js
@@ -1,11 +1,19 @@
 //rooms routes
-const express = require('express');
+const express = require("express");
 const rooms = express.Router();
-const {createRoom,SearchRoom,JoinRoom,LeaveRoom, getRoom} =require("../controllers/roomController");
+const {
+  createRoom,
+  SearchRoom,
+  JoinRoom,
+  LeaveRoom,
+  getRoom,
+  sendInvoice,
+} = require("../controllers/roomController");
 
-rooms.post("/create",createRoom);
-rooms.get("/search",SearchRoom);
-rooms.get("/JoinRoom",JoinRoom);
-rooms.get("/leave",LeaveRoom);
-rooms.get("/getRoom",getRoom);
-module.exports=rooms; 
+rooms.post("/create", createRoom);
+rooms.get("/search", SearchRoom);
+rooms.get("/JoinRoom", JoinRoom);
+rooms.get("/leave", LeaveRoom);
+rooms.get("/getRoom", getRoom);
+rooms.post("/send-invoice", sendInvoice);
+module.exports = rooms;
