9fa998e3639b0efd24095cf5bcefbf282f5f7d78
ameerhamza-23 <hamzaiscoding@gmail.com>
2024-05-05 12:43:14 +0500

    Modified Document Controller

--- routes/document.js
+++ routes/document.js
@@ -6,5 +6,8 @@ document.get('/getDocuments', documentController.getDocuments);
 document.post('/uploadDocument', documentController.uploadDocument);
 document.get('/getTrendingContributors', documentController.getTrendingContributors);
 document.post('/followContributor', documentController.followContributor);
+document.get('/incrementCounter', documentController.incrementCounter);
+document.get('/getFollowerDocuments', documentController.getFollowerDocuments);
+document.get('/getLatestDocuments', documentController.getLatestDocuments);
 
 module.exports = document;
